88afd129805cec49ba74905146c37aa00e29f005
megha angadi <meghamangadi@gmail.com>
2021-08-27 21:26:28 +0530

    Added: Program of FlipCoinTest

---
+++ FlipCoinTest.java
@@ -0,0 +1,37 @@
+package com.blz.coreprograms;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class FlipCoinTest {
+
+	public static void main(String[] args) {
+		int coin, flip, headcount = 0, tailcount = 0;
+
+		Scanner sc = new Scanner(System.in);
+		Random rand = new Random();
+
+		System.out.println("Enter number of times you want to flip coin");
+		flip = sc.nextInt();
+		sc.close();
+
+		if (flip < 0) {
+			System.out.println("Error enter a number greater than zero!");
+		} else {
+			for (int i = 0; i < flip; i++) {
+				coin = rand.nextInt(2);
+				if (coin == 0) {
+					System.out.println("HEAD");
+					headcount++;
+				} else {
+					System.out.println("TAIL");
+					tailcount++;
+				}
+			}
+			System.out.println("Percentage of Heads: " + headcount * 100 / flip);
+			System.out.println("Percentage of Tails: " + tailcount * 100 / flip);
+		}
+
+	}
+
+}
