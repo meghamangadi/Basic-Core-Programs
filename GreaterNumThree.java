72be9bd389be077cd3edf1659b5254fb3b604b1a
megha angadi <meghamangadi@gmail.com>
2021-08-27 21:30:21 +0530

    Added: Program to check greater number among three numbers

---
+++ GreaterNumThree.java
@@ -0,0 +1,30 @@
+package com.blz.coreprograms;
+
+import java.util.Scanner;
+
+public class GreaterNumThree {
+
+	public static void main(String[] args) {
+		int num1, num2, num3;
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the First Number");
+		num1 = sc.nextInt();
+
+		System.out.println("Enter the Second Number");
+		num2 = sc.nextInt();
+
+		System.out.println("Enter the Third Number");
+		num3 = sc.nextInt();
+		sc.close();
+		if (num1 > num2 && num1 > num3) {
+			System.out.println("First Number is greater");
+		} else if (num2 > num1 && num2 > num3) {
+			System.out.println("Second Number is greater");
+		} else {
+			System.out.println("Third Number is greater");
+		}
+
+	}
+
+}
