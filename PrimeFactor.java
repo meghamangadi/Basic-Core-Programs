02cbca17f7c37e8e843f4215a3c8af6289f7e7b9
megha angadi <meghamangadi@gmail.com>
2021-08-28 16:57:54 +0530

    Added: Program to print Prime factors of number

---
+++ PrimeFactor.java
@@ -0,0 +1,26 @@
+package com.blz.coreprograms;
+
+import java.util.Scanner;
+
+public class PrimeFactor {
+
+	public static void main(String[] args) {
+		int num, div;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		num = sc.nextInt();
+		System.out.print("Prime factors of given number--> ");
+
+		for (div = 2; div * div <= num; div++) {
+			while (num % div == 0) {
+				num = num / div;
+				System.out.print(div + "  ");
+			}
+
+		}
+		if (num != 1) {
+			System.out.println(num);
+		}
+	}
+
+}
