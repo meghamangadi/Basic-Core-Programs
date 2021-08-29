e92fd050208235e2d6246e9c1c6d36730cadd01a
megha angadi <meghamangadi@gmail.com>
2021-08-29 13:25:17 +0530

    Added: if statement in line no-14 to ensure it is a 4 digit number

--- LeapYear.java
+++ LeapYear.java
@@ -10,10 +10,14 @@ public class LeapYear {
 		System.out.println("Enter the year");
 		year = sc.nextInt();
 		sc.close();
-		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-			System.out.println("yes! it is leap year");
+
+		if (year > 999 && year < 9999) {
+			if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+				System.out.println("yes! it is leap year");
+			else
+				System.out.println("No, it is not a leap year");
 		} else {
-			System.out.println("No, it is not a leap year");
+			System.out.println("Enter valid year");
 		}
 
 	}
